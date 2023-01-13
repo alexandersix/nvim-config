90d68c37fa5c161bfcbdd5ae174ac14c7bbcf601
Alexander Six <alexanderhsix@gmail.com>
2023-01-13 11:33:00 -0500

    remove MRU for entire system from dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -3,7 +3,29 @@ return {
         "goolord/alpha-nvim",
         dependencies = {"kyazdani42/nvim-web-devicons"},
         config = function()
-            require("alpha").setup(require("alpha.themes.startify").config)
+            local startify = require("alpha.themes.startify")
+            require("alpha").setup({
+                layout = {
+                    { type = "padding", val = 1 },
+                    startify.section.header,
+                    { type = "padding", val = 2 },
+                    startify.section.top_buttons,
+                    startify.section.mru_cwd,
+                    { type = "padding", val = 1 },
+                    startify.section.bottom_buttons,
+                    startify.section.footer,
+                },
+                opts = {
+                    margin = 3,
+                    redraw_on_resize = false,
+                    setup = function()
+                        vim.api.nvim_create_autocmd('DirChanged', {
+                            pattern = '*',
+                            callback = function () require('alpha').redraw() end,
+                        })
+                    end,
+                },
+            })
         end
     }
 }
