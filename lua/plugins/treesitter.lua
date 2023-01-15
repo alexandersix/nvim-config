6442fa9701673dc4501ded7f2c51396ddde9dff6
Alexander Six <alexanderhsix@gmail.com>
2023-01-14 22:41:00 -0500

    add autotag to treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,13 +3,18 @@ return {
         "nvim-treesitter/nvim-treesitter",
         dependencies = {
             "nvim-treesitter/nvim-treesitter-textobjects",
+            "windwp/nvim-ts-autotag",
         },
         build = function()
             pcall(require("nvim-treesitter.install").update { with_sync = true })
         end,
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "bash", "c", "cpp", "git_rebase", "go", "help", "html", "javascript", "json", "lua", "php", "rust", "svelte", "tsx", "vim", "vue", "yaml"},
+                autotag = {
+                    enable = true
+                },
+                ensure_installed = { "bash", "c", "cpp", "git_rebase", "go", "help", "html", "javascript", "json", "lua",
+                    "php", "rust", "svelte", "tsx", "vim", "vue", "yaml" },
                 highlight = {
                     enable = true,
                 },
