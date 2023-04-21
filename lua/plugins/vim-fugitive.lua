88c48cf62993d2327367d82a13ca1137cfe32ce0
Alexander Six <alexanderhsix@gmail.com>
2023-04-21 10:07:59 -0400

    Add three-way git diff split keybind to Fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,3 +1,10 @@
 return {
-    {"tpope/vim-fugitive"}
+    {
+        "tpope/vim-fugitive",
+        keys = {
+            -- 3-way git merge tool
+            { "<leader>gm", ":Gvdiffsplit!<cr>" },
+        },
+        lazy = false,
+    },
 }
