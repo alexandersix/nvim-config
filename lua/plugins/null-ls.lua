d23a3715408ffc0b1beaa9580445494e4ac3011a
Alexander Six <alexanderhsix@gmail.com>
2023-07-10 20:47:16 -0400

    temporarily swap prettierd for prettier due to editorconfig bug on Linux

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,7 +5,7 @@ return {
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
-                    null_ls.builtins.formatting.prettierd.with({
+                    null_ls.builtins.formatting.prettier.with({
                         extra_filetypes = { "svelte" }
                     })
                 },
